25e2f0e393b822632aa70dd057519586ab4262b0
Napoli Quinelato <nquinelato@gmail.com>
2022-11-24 20:28:43 -0300

    novas alteraçoes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-const menuIcon = document.querySelector('.menu-icon');
-const navList = document.querySelector('.nav-list');
-//const navGhost = document.querySelector('.ghost');
+let menuIcon = document.querySelector('.menu-icon');
+let navList = document.querySelector('.nav-list');
 
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('active');
     navList.classList.toggle('active');
-    //navGhost.classList.toggle('active');
 });
