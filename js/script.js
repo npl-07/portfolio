4a6475911d6774eeb3831a1e300a4d3ac2321798
Napoli Quinelato <nquinelato@gmail.com>
2022-11-24 22:33:10 -0300

    adicionei a overlay

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let menuIcon = document.querySelector('.menu-icon');
 let navList = document.querySelector('.nav-list');
+let overLay = document.querySelector('.overlay');
 
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('active');
     navList.classList.toggle('active');
+    overLay.classList.toggle('active');
 });
